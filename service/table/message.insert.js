dd18120eed14caa9a95769ba6a4dd3a14b585b12
windowsazure <windowsazure>
2014-11-01 21:47:52 +0000

    Committing update from request /api/scmvfs/service/table/message.insert.js

--- service/table/message.insert.js
+++ service/table/message.insert.js
@@ -1,5 +1,11 @@
 function insert(item, user, request) {
-    var t=0;
     request.execute();
-
+    // Set timeout to delay the notification, to provide time for the
+    // app to be closed on the device to demonstrate toast notifications
+    push.apns.send(item.deviceToken, {
+        alert: "Toast: " + item.text,
+        payload: {
+            inAppMessage: "Hey, a new item arrived: '" + item.text + "'"
+        }
+    });
 }
