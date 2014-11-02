aa51307c1140cc365bf0553ab6e110d63babcbcc
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 00:54:13 -0400

    asfdkjsdaflkj

--- service/table/Message.insert.js
+++ service/table/Message.insert.js
@@ -2,14 +2,14 @@ function insert(item, user, request) {
     item.sender = user.userId;
     request.execute({
     	success: function(result) {
-		    var parser = require("../shared/parseMsg.js");
-		    var components = parser(item.content);
-		    var codeTable = tables.getTable('code');
+		    // var parser = require("../shared/parseMsg.js");
+		    // var components = parser(item.content);
+		    // var codeTable = tables.getTable('code');
     		console.log(result);
-    		components.forEach(function(entry, index){
-    			entry.order = index + 1;
-    		});
-    		console.log(components);
+    		// components.forEach(function(entry, index){
+    		// 	entry.order = index + 1;
+    		// });
+    		// console.log(components);
     		request.respond();
     	}
     });
