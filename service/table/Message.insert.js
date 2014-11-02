0ef9d8dd6795271be61d71d33ada786abd2bebba
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 00:51:11 -0400

    rename

--- service/table/Message.insert.js
+++ service/table/Message.insert.js
@@ -1,11 +1,11 @@
 function insert(item, user, request) {
     item.sender = user.userId;
     request.execute({
-    	success: function() {
+    	success: function(result) {
 		    var parser = require("../shared/parseMsg.js");
 		    var components = parser(item.content);
 		    var codeTable = tables.getTable('code');
-    		console.log();
+    		console.log(result);
     		components.forEach(function(entry, index){
     			entry.order = index + 1;
     		});
