12547bf5343220759d5226e3aa34204a6e07d243
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 01:02:10 -0400

    id changes automatically?

--- service/table/Message.insert.js
+++ service/table/Message.insert.js
@@ -1,11 +1,11 @@
 function insert(item, user, request) {
     item.sender = user.userId;
     request.execute({
-    	success: function(result) {
+    	success: function() {
 		    // var parser = require("../shared/parseMsg.js");
 		    // var components = parser(item.content);
 		    // var codeTable = tables.getTable('code');
-    		console.log(result);
+    		console.log(item.id);
     		// components.forEach(function(entry, index){
     		// 	entry.order = index + 1;
     		// });
