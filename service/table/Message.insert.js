84d554117996b050986482dc721be1060ad21277
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 01:05:18 -0400

    finish processing component

--- service/table/Message.insert.js
+++ service/table/Message.insert.js
@@ -1,15 +1,15 @@
 function insert(item, user, request) {
     item.sender = user.userId;
+	var parser = require("../shared/parseMsg.js");
     request.execute({
     	success: function() {
-		    // var parser = require("../shared/parseMsg.js");
-		    // var components = parser(item.content);
-		    // var codeTable = tables.getTable('code');
-    		console.log(item.id);
-    		// components.forEach(function(entry, index){
-    		// 	entry.order = index + 1;
-    		// });
-    		// console.log(components);
+		    var components = parser(item.content);
+		    var codeTable = tables.getTable('code');
+    		components.forEach(function(entry, index){
+    			entry.order = index + 1;
+    			entry.message_id = item.id;
+    		});
+    		console.log(components);
     		request.respond();
     	}
     });
