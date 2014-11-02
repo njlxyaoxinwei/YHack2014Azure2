a873d528cc4150b1e32d88e7b3f9752a4243d35f
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 00:49:07 -0400

    removed result

--- service/table/message.insert.js
+++ service/table/message.insert.js
@@ -1,19 +1,16 @@
 function insert(item, user, request) {
     item.sender = user.userId;
     request.execute({
-    	success: function(result) {
+    	success: function() {
 		    var parser = require("../shared/parseMsg.js");
 		    var components = parser(item.content);
 		    var codeTable = tables.getTable('code');
-    		console.log(result);
+    		console.log();
     		components.forEach(function(entry, index){
     			entry.order = index + 1;
     		});
     		console.log(components);
     		request.respond();
-    	},
-    	error: function(err) {
-    		console.log(err);
     	}
     });
 }
