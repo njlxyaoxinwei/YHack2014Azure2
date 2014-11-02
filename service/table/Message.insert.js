f6d4e1e77b7641c1c9621ae9001f3bd43aecdbf8
David Yao <njlxyaoxinwei@gmail.com>
2014-11-02 01:25:18 -0400

    batchInsert codes 2

--- service/table/Message.insert.js
+++ service/table/Message.insert.js
@@ -3,15 +3,19 @@ function insert(item, user, request) {
 	var parser = require("../shared/parseMsg").parser;
 	var batchInsert = require("../shared/batchInsert").batchInsert;
 	var codeTable = tables.getTable('code');
-    request.execute({
-    	success: function() {
-			var components = parser(item.content);
-			components.forEach(function(entry, index){
-				entry.order = index + 1;
-				entry.message_id = item.id;
-			});
-			batchInsert(codeTable, components, components.length, 10);
-	   		request.respond();
-    	}
-    });
+	var components = parser(item.content);
+	if (components) {
+	    request.execute({
+	    	success: function() {
+				components.forEach(function(entry, index){
+					entry.order = index + 1;
+					entry.message_id = item.id;
+				});
+				batchInsert(codeTable, components, components.length, 10);
+		   		request.respond();
+	    	}
+	    });		
+	} else {
+		request.respond(new Error "invalid content");
+	}
 }
